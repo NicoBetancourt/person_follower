80d8eb1e71eb18b8f5e55f8b2e86720ce407b77f
Nico Betancourt <110423467+NicoBetancourt@users.noreply.github.com>
2024-03-11 19:51:53 +0100

    Update person_follower.py

--- person_follower/person_follower.py
+++ person_follower/person_follower.py
@@ -47,20 +47,26 @@ class PersonFollower(Node):
             error_angle = target_angle - min_index
             
             # Control proporcional para ajustar las velocidades lineal y angular
-            if (-90 <= error_angle <= 90):
-                angular_velocity = 0.2*(error_angle/90) 
-                linear_velocity = 0.2*min_value/2.5
+            if abs(error_angle) <= 35 and min_value <=1.5:
+                if abs(error_angle) <= 20:
+                    angular_velocity = 0.2*(error_angle/90)
+                    linear_velocity = (1 - np.exp(-0.693 * min_value))/2.4
+                else:
+                    angular_velocity = 2.5*(error_angle/90)
+                    linear_velocity = (1 - np.exp(-0.693 * min_value))/3.2
             else:
                 linear_velocity  = 0.
                 angular_velocity = 0.                 
 
-            print("Ángulo:", min_index, "Distancia:", round(min_value, 3), "Vel Lin:", round(linear_velocity, 4), "Vel Ang:", round(angular_velocity, 4))
+            # print("Ángulo:", min_index, "Distancia:", round(min_value, 3), "Vel Lin:", round(linear_velocity, 4), "Vel Ang:", round(angular_velocity, 4))
+            print("error_angle:", round(error_angle, 3),"min_index:", round(min_index, 3),"min_value:", round(min_value, 3),"target_angle:", round(target_angle, 3))
 
             return linear_velocity, angular_velocity
-
+        target_value = 180
+        margin_value = 35
         np_ranges = np.array(ranges)
-        
-        linear_velocity, angular_velocity = follow_target(np_ranges)
+        np_ranges = np_ranges[target_value - margin_value:target_value + margin_value+1]
+        linear_velocity, angular_velocity = follow_target(np_ranges,len(np_ranges)/2)
 
         vx = linear_velocity #0.1
         wz = angular_velocity #0.1 #pos izq, neg der
